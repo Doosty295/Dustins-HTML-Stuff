7f223ca3ee630f3e1f6df23bc0ea688703092853
Dustin LeGars <dlegars1976@gmail.com>
2019-01-25 12:49:55 -0500

    stuff

---
+++ JavaScript_FunctionLibrary.js
@@ -0,0 +1,7 @@
+/* This JavaScript file exists to act as a combined
+** Repository of self-contained functions that I find
+** Particularly useful.
+** Ones I've made will be marked 'JSF-##'
+** Functions from external sources will have the source cited in single quotes.
+** Function Names will be in the format of '[CITE or JSF-##] | [Function Name]'
+*/
